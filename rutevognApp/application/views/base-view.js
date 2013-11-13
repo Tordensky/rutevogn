68703e6236d9c35ca483633db697a4f3b5d186a8
Simon Nistad <simon.nistad@hotmail.com>
2013-11-13 13:10:57 +0100

    Added a todo for Simen to add mustache template loader

--- rutevognApp/application/views/base-view.js
+++ rutevognApp/application/views/base-view.js
@@ -1,6 +1,10 @@
 var RuteVogn = RuteVogn || {};
 
 RuteVogn.BaseView = Backbone.View.extend({
+    getTemplate: function(templateName) {
+        // TODO something
+    },
+
     render: function() {
         console.log("render main view")
     }
--- rutevognApp/application/views/main-view.js
+++ rutevognApp/application/views/main-view.js
@@ -1,6 +1,8 @@
 var RuteVogn = RuteVogn || {};
 
 RuteVogn.MainView = RuteVogn.BaseView.extend({
+    template: "",
+
     initalize: function() {
         console.log("init main view");
     }
