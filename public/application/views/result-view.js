1460e18b3ab4acc2b9da9dd0df6bb6c83d7897ad
Simon Nistad <simon.nistad@hotmail.com>
2013-11-18 19:41:15 +0100

    updatet layout and fixed counter in departure table

--- public/application/views/result-view.js
+++ public/application/views/result-view.js
@@ -16,6 +16,8 @@ RuteVogn.ResultView = RuteVogn.BaseView.extend({
 
         this.fromId = null;
         this.toId = null;
+
+        this.hasUpdatetTableForThisMinutte = false;
     },
 
     showView: function(fromId, toId) {
@@ -42,31 +44,38 @@ RuteVogn.ResultView = RuteVogn.BaseView.extend({
         var that = this;
 
         var currentTimeInMs = new Date().getTime();
+        var showNumberOfResults = 7;
+
         this.travelInfoCollection.each(function(route) {
             var depTime = new Date(route.get("date"));
             var arrTime = new Date(route.get("arrival"));
             var routeNum = route.get("route");
-            var travelTime = that.createRemainingTimeStringFromMs(depTime.getTime() - currentTimeInMs, false);
+            var travelTime = that.createRemainingTimeString(depTime.getTime() - currentTimeInMs, false);
 
-            if (depTime.getTime() >= currentTimeInMs){
+            if (depTime.getTime() >= currentTimeInMs && showNumberOfResults > 0){
+                showNumberOfResults -= 1;
                 data.push({
                     depTimeInMs: depTime.getTime(),
                     route: routeNum.toString(),
                     departureTime: that.getTimeString(depTime),
                     arrivalTime: that.getTimeString(arrTime),
-                    travelTime: travelTime.toString()
+                    travelTime: travelTime.toString(),
+                    fromName: route.get("from"),
+                    toName: route.get("to")
                 });
             }
         });
         if (data.length > 0) {
             this.nextDepTime = data[0].depTimeInMs;
-            var depTime = this.createRemainingTimeStringFromMs(data[0].depTimeInMs - currentTimeInMs, true);
-            this.render({routes: data, nextDepartureTime: depTime});
+            var fromName = data[0]["fromName"];
+            var toName = data[0]["toName"];
+
+            var depTime = this.createRemainingTimeString(data[0].depTimeInMs - currentTimeInMs, true);
+            this.render({routes: data, nextDepartureTime: depTime, title: "Bussen min går om", from: fromName, to: toName});
         } else {
             this.nextDepTime = null;
             this.render({nextDepartureTime: "Ingen avganger"})
         }
-
     },
 
     startCountDownForNextDeparture: function() {
@@ -85,11 +94,30 @@ RuteVogn.ResultView = RuteVogn.BaseView.extend({
         }
     },
 
+    isOnWholeMinute: function (currentTimeInMs) {
+        return (Math.floor(currentTimeInMs / 1000) % 60) == 0;
+    },
+
+    hasDeparted: function (currentTimeInMs) {
+        return (this.nextDepTime - currentTimeInMs) < 0;
+    },
+
     updateCountDown: function(targetDiv) {
         var currentTimeInMs = new Date().getTime();
-        var timeRemaining = this.createRemainingTimeStringFromMs(this.nextDepTime - currentTimeInMs, true);
-        if ((this.nextDepTime - currentTimeInMs) < 0) {
+        var timeRemaining = this.createRemainingTimeString(this.nextDepTime - currentTimeInMs, true);
+
+        if (this.hasDeparted(currentTimeInMs)) {
             this.showView(this.fromId, this.toId);
+        }
+
+        // Quick fix for having count down in each table cell
+        if (this.isOnWholeMinute(currentTimeInMs)) {
+            if (this.hasUpdatetTableForThisMinutte == false) {
+                this.parseTravelInfo();
+                this.hasUpdatetTableForThisMinutte = true;
+            }
+        } else {
+            this.hasUpdatetTableForThisMinutte = false;
         }
 
         targetDiv.html(timeRemaining);
@@ -109,7 +137,7 @@ RuteVogn.ResultView = RuteVogn.BaseView.extend({
         return hh+":"+mm.toString();
     },
 
-    createRemainingTimeStringFromMs: function(ms, showSeconds) {
+    createRemainingTimeString: function(ms, showSeconds) {
         var seconds = Math.floor(ms / 1000);
 
         var hours = Math.floor(seconds / 3600);
