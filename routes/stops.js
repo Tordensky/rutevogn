e2f80b5715445dbaf3de69c70ae3c17c407a164a
Simen Johannessen <simenlomaa@gmail.com>
2013-11-20 20:30:14 +0100

    gives up sorting. fixed sentrum - uit routes

--- routes/stops.js
+++ routes/stops.js
@@ -1,13 +1,13 @@
 var Stop = require('../model/stop-model.js');
 
 exports.getBusStops = function(req, res){
-	Stop.find({}, null, {sort : {_id : 'asc'}}).populate('destinations').exec(function (err, destinations) {
+	Stop.find({}).sort({_id : 'asc'}).populate('destinations').exec(function (err, destinations) {
 		if (err){
 			winston.log('error', err);
 			res.send(400);
 		}
 		else {
-			console.log("Data %s", JSON.stringify(destinations, undefined, 2));
+			// console.log("Data %s", JSON.stringify(destinations, undefined, 2));
 			res.jsonp(destinations);
 		}
 	});
