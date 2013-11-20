48a8889d989a0283c3d4eeae63854b6dac959f99
Simen Johannessen <simenlomaa@gmail.com>
2013-11-20 19:59:36 +0100

    sorting attempt

--- routes/stops.js
+++ routes/stops.js
@@ -1,13 +1,13 @@
 var Stop = require('../model/stop-model.js');
 
 exports.getBusStops = function(req, res){
-	Stop.find().sort({sort_id: 'asc'}).populate('destinations').exec(function (err, destinations) {
+	Stop.find().populate('destinations').sort({_id: 'asc'}).exec(function (err, destinations) {
 		if (err){
 			winston.log('error', err);
 			res.send(400);
 		}
 		else {
-			//console.log("Data %s", JSON.stringify(destinations, undefined, 2));
+			console.log("Data %s", JSON.stringify(destinations, undefined, 2));
 			res.jsonp(destinations);
 		}
 	});
