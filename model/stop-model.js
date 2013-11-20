4777915094e93e5ea3427af2810eb1048302c147
Simen Johannessen <simenlomaa@gmail.com>
2013-11-20 19:50:07 +0100

    added index on sort field

--- model/stop-model.js
+++ model/stop-model.js
@@ -5,7 +5,7 @@ var StopSchema = new Schema({
     name : { type: String, required: true},
     city : { type: String, required: true},
     destinations : [{type: Schema.Types.ObjectId, ref: 'Stop'}],
-    sort_id : {type: Date, required: true}
+    sort_id : {type: Date, required: true, index: true}
 });
 
 module.exports = mongoose.model('Stop', StopSchema);
