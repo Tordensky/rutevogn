b18dca302d9f6dee001508b88492a47a27f09b0f
Simen Johannessen <simenlomaa@gmail.com>
2013-11-20 18:16:34 +0100

    test of sorting

--- routes/stops.js
+++ routes/stops.js
@@ -2,7 +2,7 @@ var Stop = require('../model/stop-model.js');
 
 
 exports.getBusStops = function(req, res){
-	Stop.find().populate('destinations').sort({name: 'asc'}).exec(function (err, destinations) {
+	Stop.find().sort({name: 'asc'}).populate('destinations').exec(function (err, destinations) {
 		if (err) winston.log('error', err);
 		// console.log("Data %s", JSON.stringify(destinations, undefined, 2));
 		res.jsonp(destinations);
