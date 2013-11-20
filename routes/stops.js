2d8ddcd92139683173878682066acb1877d87223
Simen Johannessen <simenlomaa@gmail.com>
2013-11-20 18:31:46 +0100

    updated on sorting

--- routes/stops.js
+++ routes/stops.js
@@ -2,7 +2,7 @@ var Stop = require('../model/stop-model.js');
 
 
 exports.getBusStops = function(req, res){
-	Stop.find().sort({"name": 'ascending'}).populate('destinations').exec(function (err, destinations) {
+	Stop.find().sort({name: 'ascending'}).populate('destinations').exec(function (err, destinations) {
 		if (err) winston.log('error', err);
 		// console.log("Data %s", JSON.stringify(destinations, undefined, 2));
 		res.jsonp(destinations);
