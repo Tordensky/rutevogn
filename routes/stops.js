0e33cbaa3708f76c58a7496dcda900e26df2ff26
Simen Johannessen <simenlomaa@gmail.com>
2013-11-20 20:11:43 +0100

    sortinggit add routes/stops.js git add routes/stops.js

--- routes/stops.js
+++ routes/stops.js
@@ -1,7 +1,7 @@
 var Stop = require('../model/stop-model.js');
 
 exports.getBusStops = function(req, res){
-	Stop.find().populate('destinations').sort({_id: 'asc'}).exec(function (err, destinations) {
+	Stop.find({}, null, {sort : {_id : 'asc'}}).populate('destinations').exec(function (err, destinations) {
 		if (err){
 			winston.log('error', err);
 			res.send(400);
