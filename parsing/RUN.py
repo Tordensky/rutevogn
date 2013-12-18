9c7cffbbb08328e7d54efe6c81c1bdc258ba9586
Simen Johannessen <simenlomaa@gmail.com>
2013-12-18 15:51:34 +0100

    updated run.py

--- parsing/RUN.py
+++ parsing/RUN.py
@@ -3,4 +3,4 @@ import os
 
 subprocess.call(["node", "parsing/cleanUp.js"])
 subprocess.call(["node", "parsing/setUp.js"])
-subprocess.call(["node", "parsing/parseXML.js", "2013-12-18", "2014-01-06"])
+subprocess.call(["node", "parsing/parseXML.js", "2013-12-18", "2014-01-10"])
