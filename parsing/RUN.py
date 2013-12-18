a01d5795edcc150dd123d273dca021946e1f266e
Simen Johannessen <simenlomaa@gmail.com>
2013-12-18 15:49:30 +0100

    updated with prestvannet

--- parsing/RUN.py
+++ parsing/RUN.py
@@ -3,12 +3,4 @@ import os
 
 subprocess.call(["node", "parsing/cleanUp.js"])
 subprocess.call(["node", "parsing/setUp.js"])
-
-
-
-
-
-
-
-subprocess.call(["node", "parsing/parseXML.js", "2013-12-05", "2013-12-24"])
-
+subprocess.call(["node", "parsing/parseXML.js", "2013-12-18", "2014-01-06"])
