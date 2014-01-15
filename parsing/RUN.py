b1ea040f309450a94f2991454167bdb5f70908c0
root <root@serv5.cs.uit.no>
2014-01-15 22:25:33 +0100

    fixed the same python issue again

--- parsing/RUN.py
+++ parsing/RUN.py
@@ -6,4 +6,4 @@ future_date = (datetime.datetime.now() + datetime.timedelta(days=4)).date()
 
 subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/cleanUp.js"])
 subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/setUp.js"])
-subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/parseXML.js", todays_date, future_date])
+subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/parseXML.js", str(todays_date), str(future_date)])
