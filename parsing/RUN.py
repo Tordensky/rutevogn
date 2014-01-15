19ed14f8c762f692f4a26eb26fc6d15a12f7a728
asv009@post.uit.no <asv009@post.uit.no>
2014-01-15 21:40:58 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	parsing/RUN.py

--- parsing/RUN.py
+++ parsing/RUN.py
@@ -1,13 +1,9 @@
 import subprocess
 import datetime
 
-subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/cleanUp.js"])
-subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/setUp.js"])
-subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/parseXML.js", "2014-1-15", "2014-1-30"])
-
 todays_date = datetime.datetime.now().date()
 future_date = (datetime.datetime.now() + datetime.timedelta(days=4)).date()
 
-subprocess.call(["node", "parsing/cleanUp.js"])
-subprocess.call(["node", "parsing/setUp.js"])
-subprocess.call(["node", "parsing/parseXML.js", todays_date, future_date])
+subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/cleanUp.js"])
+subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/setUp.js"])
+subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/parseXML.js", todays_date, future_date])
