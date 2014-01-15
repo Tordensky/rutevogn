3e27c7a702ac21200da287b5f97b1a0fa796d56e
root <root@serv5.cs.uit.no>
2014-01-15 22:22:37 +0100

    fixed a python string issue

--- parsing/RUN.py
+++ parsing/RUN.py
@@ -3,4 +3,4 @@ import os
 
 subprocess.call(["node", "parsing/cleanUp.js"])
 subprocess.call(["node", "parsing/setUp.js"])
-subprocess.call(["node", "parsing/parseXML.js", "2013-12-18", "2014-01-10"])
+subprocess.call(["node", "parsing/parseXML.js", "2014-01-05", "2014-01-31"])
