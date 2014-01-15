c9a7e6c506599255e1e5fdc28d38abf90f19b056
asv009@post.uit.no <asv009@post.uit.no>
2014-01-15 22:19:36 +0100

    new script added

---
+++ parsing/clearALL.js
@@ -0,0 +1,19 @@
+//SHOULD ONLY BE USED FOR TEST PURPOSES
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/rutevogn');
+
+var Depature = require('../model/depature-model');
+var Stop = require('../model/stop-model.js');
+
+Depature.remove({}, function(err) {
+    console.log('collection removed');
+    removeStop();
+});
+
+function removeStop(){
+    Stop.remove({}, function(err){
+        console.log('collection removed');
+        process.exit(0);
+    });
+}
