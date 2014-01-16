a9aab0feae3cc9a0ccf93b1dfd016b614118e058
Simen Johannessen <simenlomaa@gmail.com>
2014-01-16 13:02:37 +0100

    run fixed for local running

--- parsing/RUN.py
+++ parsing/RUN.py
@@ -4,6 +4,11 @@ import datetime
 todays_date = datetime.datetime.now().date()
 future_date = (datetime.datetime.now() + datetime.timedelta(days=4)).date()
 
-subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/cleanUp.js"])
-subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/setUp.js"])
-subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/parseXML.js", todays_date, future_date])
+# subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/cleanUp.js"])
+# subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/setUp.js"])
+# subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/parseXML.js", str(todays_date), str(future_date)])
+
+
+subprocess.call(["node", "parsing/cleanUp.js"])
+subprocess.call(["node", "parsing/setUp.js"])
+subprocess.call(["node", "parsing/parseXML.js", str(todays_date), str(future_date)])
