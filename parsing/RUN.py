42ccbc418768b29db458c3fae2ebbfc1c33cf747
Simen Johannessen <simenlomaas@gmail.com>
2014-08-16 18:38:51 +0200

    update onclick header and RUN py to correct path

--- parsing/RUN.py
+++ parsing/RUN.py
@@ -2,7 +2,7 @@ import subprocess
 import datetime
 
 todays_date = datetime.datetime.now().date()
-future_date = (datetime.datetime.now() + datetime.timedelta(days=4)).date()
+future_date = (datetime.datetime.now() + datetime.timedelta(days=3)).date()
 
 
 # For local testing
@@ -10,6 +10,6 @@ future_date = (datetime.datetime.now() + datetime.timedelta(days=4)).date()
 # subprocess.call(["node", "parsing/setUp.js"])
 # subprocess.call(["node", "parsing/parseXML.js", str(todays_date), str(future_date)])
 
-subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/cleanUp.js"])
-subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/setUp.js"])
-subprocess.call(["node", "/home/rutevogn/rutevogn/parsing/parseXML.js", str(todays_date), str(future_date)])
+subprocess.call(["node", "rutevogn/parsing/cleanUp.js"])
+subprocess.call(["node", "rutevogn/parsing/setUp.js"])
+subprocess.call(["node", "rutevogn/parsing/parseXML.js", str(todays_date), str(future_date)])
