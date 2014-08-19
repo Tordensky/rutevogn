3cc97f2f5cfcc0c4912d295c59bb2061cd54265c
Simen Johannessen <simenlomaas@gmail.com>
2014-08-19 18:31:45 +0200

    after merge

--- parsing/cleanUp.js
+++ parsing/cleanUp.js
@@ -5,7 +5,13 @@ var Depature = require('../model/depature-model');
 var Stop = require('../model/stop-model.js');
 var date = new Date()
 console.log(date);
-Depature.remove({date : {$lt : date}}, function(err) { 
-	console.log('collection removed lesser than ', date, 'removed');
-	process.exit(0);
+// Depature.remove({date : {$lt : date}}, function(err) { 
+// 	console.log('collection removed lesser than ', date, 'removed');
+// 	process.exit(0);
+// });
+Depature.remove(function(err){
+
+});
+Stop.remove(function(err){
+
 });
